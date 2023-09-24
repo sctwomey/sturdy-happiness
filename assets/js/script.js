d4a4b995a76c973724d6023a0338924008aade63
sctwomey <sctwomey@gmail.com>
2023-09-23 17:07:24 -0700

    Add toTitle function to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,14 @@ let options = { // This provides options for the date constructor.
     month: 'long',
     day: 'numeric'
 };
+
+// This is a function to convert the first letters of words to an uppercase and the rest of the letters to lowercase as in titles (from code.tutsplus.com).
+function toTitle(str) {
+    let splitStr = str.toLowerCase().split(' ');
+
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    };
+    return splitStr.join(' ');
+
+};
